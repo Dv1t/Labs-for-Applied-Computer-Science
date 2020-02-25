aadb49c7b042c6793dc0c4d9f7f01d706ea8f3cf
Dv1t <vitdrav@gmail.com>
2020-02-25 15:53:59 +0300

    lab2

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Matrix m1;
+            Matrix m2;
+            Console.WriteLine("Ввод первой матрицы...");
+            try
+            {
+                m1 = new Matrix();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Main();
+                return;
+            }
+            float d = m1.GetDeterminant(m1);
+            Console.WriteLine("Ввод второй матрицы...");
+            try
+            {
+                m2 = new Matrix();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Main();
+                return;
+            }
+            Matrix m3 = m1.Add(m2);
+            Matrix m4 = m1.Multiply(m2);
+            try
+            {
+                Matrix mr = m1.GetOpposite();
+                Console.WriteLine($"Обратная к первой матрица\n{mr.ToString()}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine($"Первая матрица \n{m1.ToString()}");
+            Console.WriteLine($"Детерминант первой матрицы {d}");
+            Console.WriteLine($"Вторая матрица \n{m2.ToString()}");
+            Console.WriteLine($"Сумма первой и второй матриц \n{m3.ToString()}");
+            Console.WriteLine($"Произведение первой и второй матриц \n{m4.ToString()}");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ lab2/lab2/ZeroDeterminantException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab2
+{
+    class ZeroDeterminantException:Exception
+    {
+        private string _message = "Определитель матрицы равен нулю";
+        override
+        public string Message { get { return _message; } }
+      
+        override
+        public string ToString()
+        {
+            return ($"{_message} {StackTrace}");
+        }
+    }
+}
